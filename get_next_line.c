82ff1cd4fa2e939a93f6c4e537db6e06fc3cb27d
Manisha Shekhar Mirje <mmirje@2-D-8.42heilbronn.de>
2024-11-11 15:17:07 +0100

    worked on right_buffer function

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,39 @@
 /*   By: mmirje <mmirje@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 12:27:57 by mmirje            #+#    #+#             */
-/*   Updated: 2024/11/11 11:55:52 by mmirje           ###   ########.fr       */
+/*   Updated: 2024/11/11 15:16:38 by mmirje           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char *ft_left_buffer(char *r_buffer)
+static char	*ft_right_buffer(char *r_buffer)
+{
+	size_t	i;
+	size_t	j;
+	char	*buffer;
+
+	i = 0;
+	j = 0;
+	if (!r_buffer)
+		return (free (r_buffer), NULL);
+	while (r_buffer[i] && r_buffer[i] != '\n')
+		i++;
+	if (!r_buffer[i])
+		return (free (r_buffer), NULL);
+	buffer = (char *)ft_calloc((ft_len(r_buffer) - i) + 1, sizeof(char));
+	if (!buffer)
+		return (free (r_buffer), NULL);
+	while (r_buffer[i])
+		buffer[j++] = r_buffer[++i];
+	buffer[j] = '\0';
+	if (!*buffer)
+		return (free (buffer), free (r_buffer), NULL);
+	free (r_buffer);
+	return (buffer);
+}
+
+static char	*ft_left_buffer(char *r_buffer)
 {
 	size_t	i;
 	char	*buff;
@@ -78,3 +104,24 @@ char	*get_next_line(int fd)
 	right_buffer = ft_right_buffer(right_buffer);
 	return (left_buffer);
 }
+
+/*int	main(void)
+{
+	int		fd;
+	char	*next_line;
+	int		count;
+
+	count = 1;
+	fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+		return (1);
+	while (count < 20)
+	{
+		next_line = get_next_line(fd);
+		printf("next_line:%s", next_line);
+		free(next_line);
+		count++;
+	}
+	close(fd);
+	return (0);
+}*/
